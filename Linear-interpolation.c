f7729113c5a13744fb41dfa063160d2192b75f34
chrom-P <kkllmm104@gmail.com>
2022-01-11 12:24:00 +0900

    up

--- Linear-interpolation.c
+++ Linear-interpolation.c
@@ -2,22 +2,13 @@
 #include <stdio.h>
 
 int main(void){
-    float x0, x1, x2, f0, f1, f2, x, h, r, f;
-    
-    x0=9.0;
-    f0=2.197;
-    x1=9.5;
-    f1=2.251;
-    x=9.2;
-    x2=10.0;
-    f2=2.3026;
-    printf("input x0, f0: 9.0 2.197\n");
-
-    printf("input x1,f1 : 9.5 2.251\n");
-
-    printf("input x : 9.2\n");
-
-    printf("input x2,f2 : 10.0 2.3026\n");
+    float x0, x1, f0, f1, x, h, r, f;
+    printf("input x0, f0:");
+    scanf("%f %f",&x0,&f0);
+    printf("input x1,f1 : ");
+    scanf("%f %f",&x1,&f1);
+    printf("input x : ");
+    scanf("%f",&x);
     h=x1-x0;
     r=(x-x0)/h;
     f=f0+r*(f1-f0);
